c9e62093c4e483d53f08fda076459def74a765b4
vlad <vladsheberdas7@gmail.com>
2023-08-03 14:55:37 +0400

    Added ui tests without page object pattern

--- tests/add-product-to-cart.spec.js
+++ tests/add-product-to-cart.spec.js
@@ -43,9 +43,9 @@ test.describe("Make purchase", () => {
     await AddItemToCart(page, 1);
     await page.locator('//a[@class="shopping_cart_link"]').click();
     await page.locator('//button[@id="checkout"]').click();
-    await page.locator('//input[@id="first-name"]').fill('test');
-    await page.locator('//input[@id="last-name"]').fill('test');
-    await page.locator('//input[@id="postal-code"]').fill('0000');
+    await page.locator('//input[@id="first-name"]').fill("test");
+    await page.locator('//input[@id="last-name"]').fill("test");
+    await page.locator('//input[@id="postal-code"]').fill("0000");
     await page.locator('//input[@type="submit"]').click();
     await page.locator('//button[@id="finish"]').click();
 
@@ -55,29 +55,59 @@ test.describe("Make purchase", () => {
 });
 
 test.describe("Verify social media", () => {
-    test.beforeEach(async ({ page }) => {
-      await page.goto("https://www.saucedemo.com/");
-      await page.locator('//input[@id="user-name"]').fill("standard_user");
-      await page.locator('//input[@id="password"]').fill("secret_sauce");
-      await page.locator('//input[@id="login-button"]').click();
-    });
-  
-    test("should open Twitter", async ({ page }) => {
-        await page.locator('//li[@class="social_twitter"]').click();
-        await expect(page).toHaveURL('https://twitter.com/saucelabs');
-    });
-
-    test("should open Facebook", async ({ page }) => {
-        await page.locator('//li[@class="social_facebook"]').click();
-        await expect(page).toHaveURL('https://www.facebook.com/saucelabs');
-    });
-
-    test("should open LinkedIn", async ({ page }) => {
-        await page.locator('//li[@class="social_linkedin"]').click();
-        await expect(page).toHaveURL('https://www.linkedin.com/company/sauce-labs/');
-    });
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator('//input[@id="user-name"]').fill("standard_user");
+    await page.locator('//input[@id="password"]').fill("secret_sauce");
+    await page.locator('//input[@id="login-button"]').click();
   });
 
+  test("should open Twitter", async ({ page, context }) => {
+    const pagePromise = context.waitForEvent("page");
+    await page.locator('//li[@class="social_twitter"]').click();
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+    await expect(newPage).toHaveURL("https://twitter.com/saucelabs");
+  });
+
+  test("should open Facebook", async ({ page, context }) => {
+    const pagePromise = context.waitForEvent("page");
+    await page.locator('//li[@class="social_facebook"]').click();
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+    await expect(newPage).toHaveURL("https://www.facebook.com/saucelabs");
+  });
+
+  test("should open LinkedIn", async ({ page, context }) => {
+    const pagePromise = context.waitForEvent("page");
+    await page.locator('//li[@class="social_linkedin"]').click();
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+    await expect(newPage).toHaveURL(
+      "https://www.linkedin.com/company/sauce-labs/"
+    );
+  });
+});
+
+test.describe("Verify sorting by item price", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator('//input[@id="user-name"]').fill("standard_user");
+    await page.locator('//input[@id="password"]').fill("secret_sauce");
+    await page.locator('//input[@id="login-button"]').click();
+  });
+
+  test("should sort products by price from low to high", async ({ page }) => {
+    await page
+      .locator('//select[@data-test="product_sort_container"]')
+      .selectOption({ value: "lohi" });
+    const itemPriceArray = await page.$$(
+      '//div[@class="inventory_item_price"]'
+    );
+
+    expect(isSortedAscending(itemPriceArray)).toBeTruthy();
+  });
+});
 
 async function AddItemToCart(page, index) {
   const itemLocator = await page.locator(
@@ -85,4 +115,13 @@ async function AddItemToCart(page, index) {
   );
 
   await itemLocator.click();
+}
+
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
 }
